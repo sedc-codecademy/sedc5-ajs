4e087501f853baf009985510b4173a6da51872fa
Wekoslav Stefanovski <swekster@gmail.com>
2017-01-11 19:46:05 +0100

    Exercise done

--- group-2/exercise-1/script.js
+++ group-2/exercise-1/script.js
@@ -12,3 +12,24 @@ let greetings = [
         person: "officer"
     }
 ]
+
+function sayHello(greeting, person){
+    let result = document.getElementById("result");
+    let date = new Date();
+    date = `${date.getSeconds()}:${date.getMilliseconds()}`;
+    result.innerHTML += `${greeting}, ${person}! (${date})<br/>`;
+}
+
+setTimeout(function() {
+    sayHello(greetings[0].greeting, greetings[0].person);
+    setTimeout(function() {
+        sayHello(greetings[2].greeting, greetings[2].person);
+        setTimeout(function() {
+            sayHello(greetings[1].greeting, greetings[1].person);
+        }, 1000);
+        sayHello(greetings[2].greeting, greetings[2].person);
+    }, 4000);
+    sayHello(greetings[0].greeting, greetings[0].person);
+}, 2000);
+
+sayHello(greetings[1].greeting, greetings[1].person);
