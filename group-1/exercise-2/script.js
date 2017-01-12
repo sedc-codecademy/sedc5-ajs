d835b337551724865312a7d9a6a77c166d50e3c0
Wekoslav Stefanovski <swekster@gmail.com>
2017-01-12 20:38:33 +0100

    added fill tables

--- group-1/exercise-2/script.js
+++ group-1/exercise-2/script.js
@@ -34,4 +34,15 @@ let albums = [
         album: "Persistence of time",
         duration: 3700
     }
-]
+];
+
+document.getElementById("fill").addEventListener("click", ()=>{
+    let body = document.getElementsByTagName("tbody")[0];
+    for (var index = 0; index < albums.length; index++) {
+        var album = albums[index];
+        body.rows[index].cells[0].textContent = album.artist;
+        body.rows[index].cells[1].textContent = album.album;
+        body.rows[index].cells[2].textContent = album.duration;
+    }
+});
+
