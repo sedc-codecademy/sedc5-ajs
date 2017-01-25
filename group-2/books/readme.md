494d9094480a320a5fa9e7bb8028bfc3caf3f23b
Wekoslav Stefanovski <swekster@gmail.com>
2017-01-25 20:37:03 +0100

    Task 4, solution 3

--- group-2/books/readme.md
+++ group-2/books/readme.md
@@ -5,3 +5,5 @@
 2. For each item in the json array, display a row in the table.
 
 3. Add paging to the page, along with previous and next buttons
+
+4. Display only books with awards
--- group-2/books/script.js
+++ group-2/books/script.js
@@ -1,16 +1,55 @@
+let booksContainer = $("#booksContainer");
+
+let addBookToTable = function (book, booksContainer) {
+    booksContainer.append(`<tr>
+                    <td>${book.author}</td>
+                    <td>${book.title}</td>
+                    <td>${book.awards}</td>
+                    <td><img src="https://www.worldswithoutend.com/${book.img}" /></td>
+                </tr>`);
+};
+
+let pageSize = 20;
+let pageNumber = 1;
+let books = [];
+
+let removeRows = function (booksContainer) {
+    booksContainer.html("");
+}
+
+let displayPage = (pageNumber, pageSize, books, booksContainer) => {
+    removeRows(booksContainer);
+    let startIndex = (pageNumber - 1) * pageSize;
+    let endIndex = pageNumber * pageSize;
+    let displayBooks = books.slice(startIndex, endIndex)
+    displayBooks.forEach(b => addBookToTable(b, booksContainer))
+}
+
 $(() => {
+    $("#previous").on('click', () => {
+        if (pageNumber > 1) {
+            pageNumber -= 1;
+        }
+        displayPage(pageNumber, pageSize, books, booksContainer);
+    })
+
+    $("#next").on('click', () => {
+        let maxPageNumber = (books.length / pageSize | 0) + 1;
+        if (pageNumber < maxPageNumber) {
+            pageNumber += 1;
+        }
+        displayPage(pageNumber, pageSize, books, booksContainer);
+    })
+
     $.ajax("books2.json", {
         complete: (data) => {
             let result = data.responseJSON;
-            let booksContainer = $("#booksContainer");
-            result.forEach(b => {
-                booksContainer.append(`<tr>
-                    <td>${b.author}</td>
-                    <td>${b.title}</td>
-                    <td>${b.awards}</td>
-                    <td><img src="https://www.worldswithoutend.com/${b.img}" /></td>
-                </tr>`);
-            })
+            books = result;
+            displayPage(pageNumber, pageSize, books, booksContainer);
         }
-    })
+    });
 })
+
+
+
+
