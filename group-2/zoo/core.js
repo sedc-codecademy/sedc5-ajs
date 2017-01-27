4c3ea1e6ea5e301a5a986c96e7d7caf306d9c8da
Wekoslav Stefanovski <swekster@gmail.com>
2017-01-27 18:44:46 +0100

    added food in core

--- group-2/zoo/core.js
+++ group-2/zoo/core.js
@@ -1,1 +1,20 @@
 let animals = [];
+
+let salad = {
+    name: "salad",
+    type: "grass"
+}
+
+let venison = {
+    name: "venison",
+    type: "meat"
+}
+
+let bunny;
+let wolf;
+
+bunny.eat(salad); //expected: Bunny is eating salad
+bunny.eat(venison); //expected: ERROR: Bunny cannot eat venison
+
+wolf.eat(salad); //expected: ERROR: Wolf cannot eat salad
+wolf.eat(venison); //expected: Wolf is eating venison
